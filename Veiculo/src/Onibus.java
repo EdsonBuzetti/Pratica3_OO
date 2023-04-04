c791489d4878839bc4d8be25e80cc85256f10e67
EdsonBuzetti <126629330+EdsonBuzetti@users.noreply.github.com>
2023-04-04 14:06:47 -0300

    Add files via upload

---
+++ Veiculo/src/Onibus.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+public final class Onibus extends Veiculo {
+    private int assentos;
+
+    public Onibus(String placa, int ano, int assentos) {
+        super(placa, ano);
+        this.assentos = assentos;
+    }
+
+    public int getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(int assentos) {
+        this.assentos = assentos;
+    }
+
+    public String exibirDados() {
+        String mensagem = super.exibirDados() + "\nAssentos: " + getAssentos();
+        return mensagem;
+    }
+}
---
+++ Veiculo/src/Veiculo.java
@@ -0,0 +1,21 @@
+public class Veiculo {
+    private String placa;
+    private int ano;
+
+    public Veiculo(String placa, int ano) {
+        this.placa = placa;
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String exibirDados() {
+        return null;
+    }
+}
